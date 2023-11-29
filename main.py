02ecd4830458ad62e77c1405064f71803eb90f22
Egor Devyatkin <inkey.cherry@gmail.com>
2023-11-29 13:42:02 +0300

    fix - 2

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import numpy as np
 
 
-def bernoulli_criterion(matrix):
+def bernoulli(matrix):
     row_with_max_mean = np.argmax(np.mean(matrix, axis=1))
     max_mean = np.max(np.mean(matrix, axis=1))
     return row_with_max_mean + 1, max_mean
 
 
-def wald_criterion(matrix):
+def wald(matrix):
     min_values = np.min(matrix, axis=1)
     max_min_value = np.max(min_values)
     row_with_max_min_value = np.argmax(min_values)
     return row_with_max_min_value + 1, max_min_value
 
 
-def max_criterion(matrix):
+def max(matrix):
     max_values = np.max(matrix, axis=1)
     max_max_value = np.max(max_values)
     row_with_max_max_value = np.argmax(max_values)
     return row_with_max_max_value + 1, max_max_value
 
 
-def hurwicz_criterion(matrix, a):
+def hurwicz(matrix, a):
     min_values = np.min(matrix, axis=1)
     max_values = np.max(matrix, axis=1)
     row_with_best_value = np.argmax(a * min_values + (1 - a) * max_values)
@@ -29,7 +29,7 @@ def hurwicz_criterion(matrix, a):
     return row_with_best_value + 1, hurwicz
 
 
-def savage_criterion(matrix):
+def savage(matrix):
     max_values = np.max(matrix, axis=1)
     min_max_value = np.min(max_values)
     row_with_min_max_value = np.argmin(max_values)
@@ -55,12 +55,12 @@ alpha = 0.5
 
 print("Начальная матрица стратегий:", "\n", game_matrix)
 
-print(f"\nМетод Бернулли (принцип недостаточного основания): \nНомер стратегии игрока - {bernoulli_criterion(game_matrix)[0]} \nМатематическое ожидание - {bernoulli_criterion(game_matrix)[1]}")
+print(f"\nМетод Бернулли (принцип недостаточного основания): \nНомер стратегии игрока - {bernoulli(game_matrix)[0]} \nМатематическое ожидание - {bernoulli(game_matrix)[1]}")
 
-print(f"\nМетод Вальда (пессимистический): \nНомер стратегии игрока - {wald_criterion(game_matrix)[0]} \nНижняя цена игры - {wald_criterion(game_matrix)[1]}")
+print(f"\nМетод Вальда (пессимистический): \nНомер стратегии игрока - {wald(game_matrix)[0]} \nНижняя цена игры - {wald(game_matrix)[1]}")
 
-print(f"\nМетод максимума (оптимистический): \nНомер стратегии игрока - {max_criterion(game_matrix)[0]} \nМаксимальный выигрыш - {max_criterion(game_matrix)[1]}")
+print(f"\nМетод максимума (оптимистический): \nНомер стратегии игрока - {max(game_matrix)[0]} \nМаксимальный выигрыш - {max(game_matrix)[1]}")
 
-print(f"\nМетод Гурвица: \nНомер стратегии игрока - {hurwicz_criterion(game_matrix, alpha)[0]} \nОптимальное значение - {hurwicz_criterion(game_matrix, alpha)[1]}")
+print(f"\nМетод Гурвица: \nНомер стратегии игрока - {hurwicz(game_matrix, alpha)[0]} \nОптимальное значение - {hurwicz(game_matrix, alpha)[1]}")
 
-print(f"\nМетод Севиджа (критерий риска): \n{risk_table} \n\nНомер стратегии игрока - {savage_criterion(risk_table)[0]} \nТаблица максимумов - {savage_criterion(risk_table)[1]} \nОптимальное значение - {savage_criterion(risk_table)[2]}")
+print(f"\nМетод Севиджа (критерий риска): \n{risk_table} \n\nНомер стратегии игрока - {savage(risk_table)[0]} \nТаблица максимумов - {savage(risk_table)[1]} \nОптимальное значение - {savage(risk_table)[2]}")
